173074ec28e6da22657842b3ae040892c7637929
Ananya Tiwari <ananyatiwari577@gmail.com>
2025-01-15 20:08:37 +0530

    eigth commit

---
+++ Trees/LowestCommonAncestor.java
@@ -0,0 +1,28 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+    }
+}
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root; // If p and q are found in different subtrees, root is their LCA.
+        }
+
+        return left != null ? left : right; // Return the non-null subtree.
+    }
+}
