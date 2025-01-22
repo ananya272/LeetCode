6aabf22b0866e8f020a74c955c4032be91021a6d
Ananya Tiwari <ananyatiwari577@gmail.com>
2025-01-22 20:35:15 +0530

    tenth commit

---
+++ BST/BuildBST.java
@@ -0,0 +1,27 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+    }
+    }
+public class BuildBST{
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null){
+            root = new TreeNode(val);
+            return root;
+        }
+        if(root.val > val){
+            root.left = insertIntoBST(root.left,val);
+        }
+        else{
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
